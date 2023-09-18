abf547b6679c8ec3ea950207a6f54cf056840dbe
Thao <xuanthao.nguyen>
2023-09-18 17:40:34 +0700

    feat: add module users

---
+++ src/modules/user/user.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { User } from 'src/database/entities';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import FilterBuilderService from 'src/common/filter-builder/filter-builder.service';
+
+@Module({
+	imports: [TypeOrmModule.forFeature([User])],
+	controllers: [UserController],
+	providers: [UserService, FilterBuilderService],
+})
+export class UserModule {}
