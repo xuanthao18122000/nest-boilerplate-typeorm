9e8f7964ca7237623ff0813d22f133765e5ceb10
Thao <xuanthao.nguyen>
2023-09-19 09:31:12 +0700

    feat: add pre-commit when commit code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AppService } from './app.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
